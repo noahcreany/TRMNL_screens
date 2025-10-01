cd9d46207c64557dd7a85191bc053da8380f2d85
noahcreany <86799936+noahcreany@users.noreply.github.com>
2025-10-01 13:34:43 -0600

    Delete matplotlib_fonts.py

--- matplotlib_fonts.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.font_manager as fm
-
-# Get a sorted list of unique font names
-font_names = sorted(set(f.name for f in fm.fontManager.ttflist))
-
-print("--- Available Matplotlib Fonts ---")
-for name in font_names:
-    print(name)
-print("---------------------------------")
